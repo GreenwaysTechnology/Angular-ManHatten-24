42cb812bc2647fe124c3f47860fdd034814eaf1d
subugee <sasubramanian_md@hotmail.com>
2024-12-26 12:07:25 +0530

    angular modules

--- modulesapp/src/app/app.module.ts
+++ modulesapp/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { GreeterModule } from './greeter/greeter.module';
+import { HelloModule } from './hello/hello.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,GreeterModule
+    BrowserModule,GreeterModule, HelloModule
   ],
   providers: [],
   bootstrap: [AppComponent]
